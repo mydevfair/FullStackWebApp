f0f1a285a1b9b5ccf7d73377721dd97188c0e412
cfairhurst5680@gmail.com <114191776+mydevfair@users.noreply.github.com>
2024-03-19 17:04:55 +0000

    First Commit Edit View and Delete to complete

---
+++ fullStackBackEnd/fullStackBackEnd/src/main/java/com/project/fullStackBackEnd/FullStackBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project.fullStackBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackBackEndApplication.class, args);
+	}
+
+}
---
+++ fullStackBackEnd/fullStackBackEnd/src/test/java/com/project/fullStackBackEnd/FullStackBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fullStackBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
